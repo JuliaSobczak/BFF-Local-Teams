88caeee0e096fe558bc641f94f879b06bb51d361
Julia Sobczak <69363278+JuliaSobczak@users.noreply.github.com>
2020-09-01 11:26:23 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 
 const config = {
-    style: "mapbox://styles/juliasobczak/ckdkd0g5r112i1imqc8zo7165",
-    accessToken: "pk.eyJ1IjoianVsaWFzb2JjemFrIiwiYSI6ImNrY2RtOXpkZDAwenUycG54eXlsYTFmcXkifQ.3hLkp5zWDu7IvZG_zsaKJQ",
-    CSV: "https://docs.google.com/spreadsheets/d/1wJtHsBoDY2DMr-rX0jqQzZlSOvj1a55xH8aCksW04Ig/gviz/tq?tqx=out:csv&sheet=Sheet1",
+    style: "mapbox://styles/juliasobczak/ckd6fa77o03js1ik1s7upamu4"
+    accessToken: 'pk.eyJ1IjoianVsaWFzb2JjemFrIiwiYSI6ImNrY2RtOXpkZDAwenUycG54eXlsYTFmcXkifQ.3hLkp5zWDu7IvZG_zsaKJQ',
+    CSV: "https://docs.google.com/spreadsheets/d/1CA49QC4xgZH6A8WnRbCMggisY4M-MbkM8tFwckQo9kw/gviz/tq?tqx=out:csv&sheet=Pawprints",
     center: [-99.140625,
         39.90973623453719], //Lng, Lat
     zoom: 3, //Default zoom
     title: "2025 Local Action Teams",
     description: "Contact your Local Action Team for more information on how to volunteer with Best Friends",
-    sideBarInfo: ["First_Name", "Last_Name" "Contact"],
-    popupInfo: ["Name"]
+    sideBarInfo: ["First Name", "Last Name", "Primary Email", "Team Name", "City", "ST"],
+    popupInfo: ["Team Name"]
 
 };
